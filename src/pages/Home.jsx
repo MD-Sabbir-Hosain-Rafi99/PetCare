788bb849aa4c18ccdb0d16fa8b645459bbdba07e
MD-Sabbir-Hosain-Rafi99 <sabbirhossain.rafi121@gmail.com>
2025-10-25 19:46:08 +0600

    Marquee extra section design added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,8 @@ import WinterCareTips from '../layouts/WinterCareTips';
 import Experts from '../layouts/Experts';
 import { Link } from 'react-router';
 import ServiceDetailsCard from '../components/ServiceDetailsCard';
-
-
+import { FaPaw, FaDog, FaCat, FaHeart, FaBone } from "react-icons/fa";
+import '../App.css'
 
 const Home = () => {
 
@@ -30,7 +30,55 @@ const Home = () => {
                 </div>
             </div>
             <WinterCareTips />
+            <div className="marquee-container">
+                <div className="marquee-content">
+                    <div className="flex items-center gap-2">
+                        <FaPaw size={32} className="text-blue-600" />
+                        <h4 className="text-lg font-semibold">Pet Care</h4>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <FaDog size={32} className="text-green-500" />
+                        <h4 className="text-lg font-semibold">Dog's</h4>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <FaCat size={32} className="text-pink-500" />
+                        <h4 className="text-lg font-semibold">Cat's</h4>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <FaHeart size={32} className="text-red-500" />
+                        <h4 className="text-lg font-semibold">Loves</h4>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <FaBone size={32} className="text-yellow-500" />
+                        <h4 className="text-lg font-semibold">Bones</h4>
+                    </div>
 
+                    <div className="flex items-center gap-2">
+                        <FaPaw size={32} className="text-blue-600" />
+                        <h4 className="text-lg font-semibold">PetCare</h4>
+                    </div>
+
+                    <div className="flex items-center gap-2">
+                        <FaDog size={32} className="text-green-500" />
+                        <h4 className="text-lg font-semibold">Dogs</h4>
+                    </div>
+
+                    <div className="flex items-center gap-2">
+                        <FaCat size={32} className="text-pink-500" />
+                        <h4 className="text-lg font-semibold">Cats</h4>
+                    </div>
+
+                    <div className="flex items-center gap-2">
+                        <FaHeart size={32} className="text-red-500" />
+                        <h4 className="text-lg font-semibold">Loves</h4>
+                    </div>
+
+                    <div className="flex items-center gap-2">
+                        <FaBone size={32} className="text-yellow-500" />
+                        <h4 className="text-lg font-semibold">Bones</h4>
+                    </div>
+                </div>
+            </div>
             <Experts />
         </>
 
